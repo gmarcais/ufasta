e8edd98b661f50d7ad2ecf385f704a651799277e
Guillaume Marcais <gmarcais@umd.edu>
2015-02-02 14:46:06 -0500

    extract: switch to extrat random sequences.

--- extract.cc
+++ extract.cc
@@ -3,6 +3,7 @@
 #include <limits>
 #include <algorithm>
 #include <unordered_set>
+#include <random>
 
 #include "common.hpp"
 #include <extract_cmdline.hpp>
@@ -24,20 +25,33 @@ std::unordered_set<std::string> read_names(const extract_cmdline& args) {
   return res;
 }
 
+double check_proba(const extract_cmdline& args) {
+  if(!args.probabilistic_given) return 0.0;
+  if(args.probabilistic_arg <= 0.0 || args.probabilistic_arg >= 1.0)
+    extract_cmdline::error() << "Invalid probability " << args.probabilistic_arg << ", it must be in (0.0, 1.0)";
+  return args.probabilistic_arg;
+}
+
 int extract_main(int argc, char *argv[]) {
   extract_cmdline args(argc, argv);
 
-  const auto entries = read_names(args);
-  const auto end     = entries.cend();
+  const auto entries    = read_names(args);
+  const auto end        = entries.cend();
+  const bool no_entries = entries.empty();
 
   size_t max_count = args.max_count_given ? args.max_count_arg : std::numeric_limits<size_t>::max();
+  if(max_count == 0) return EXIT_SUCCESS;
+
+  std::default_random_engine             generator(time(nullptr)); // Weak random iniatilization. Ok for our purpose
+  std::uniform_real_distribution<double> dist; // uniform in [0.0, 1.0)
+  const double                           proba = check_proba(args);
+
   std::string line;
   for(auto file : args.file_arg) {
     try {
       std::ifstream is;
       is.exceptions(std::ios::failbit|std::ios::badbit);
       is.open(file);
-      bool          skip = true;
       int           c;
       // Display unchanged up to first header
       for(c = is.peek(); c != '>' && c != EOF; c = is.peek()) {
@@ -48,17 +62,18 @@ int extract_main(int argc, char *argv[]) {
       while(c != EOF) {
         std::getline(is, line);
         const auto space = line.find_first_of(" \t", 1);
-        skip = (entries.find(line.substr(1, space - 1)) == end) ^ args.invert_match_flag;
+        const bool skip = ((!no_entries && entries.find(line.substr(1, space - 1)) == end) ||
+                           (proba && dist(generator) >= proba)) ^ args.invert_match_flag;
         if(skip) {
           for(c = is.peek(); c != '>' && c != EOF; c = is.peek())
             skip_line(is);
         } else {
-          if(max_count-- == 0) break;
           std::cout << line << '\n';
           for(c = is.peek(); c != '>' && c != EOF; c = is.peek()) {
             std::getline(is, line);
             std::cout << line << '\n';
           }
+          if(--max_count == 0) break;
         }
       }
     } catch(std::ios::failure) {
--- tests/test_extract.sh
+++ tests/test_extract.sh
@@ -13,3 +13,9 @@ EXPECT_SUCCESS "Extract complement 5 reads"
 diff <(ufasta hgrep -v -m 4 '^read(4|5|7|10|32)\b' test1.fasta) \
     <(ufasta extract -v -m 4 -n read10 -n read5 -n toto -f <(echo read4 read5 blah read7) -f <(echo read32) -f /dev/null test1.fasta)
 EXPECT_SUCCESS "Extract complement 4 reads"
+
+NBT=$(grep -c '^>' test1.fasta)
+NBE=$(ufasta extract -p 0.5 test1.fasta | grep -c '^>')
+# Small probability to fail
+perl -e 'exit(!($ARGV[1] >= $ARGV[0] / 4 && $ARGV[1] <= 3 * $ARGV[0] / 4))' $NBT $NBE
+EXPECT_SUCCESS "Extract random number of reads"
