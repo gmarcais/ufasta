1dcdda3d338704a498cb19b4417cf40459bd1098
Guillaume Marcais <gmarcais@umd.edu>
2015-03-16 21:32:02 -0400

    Added -n switch to dgrep.

--- dgrep.cc
+++ dgrep.cc
@@ -22,6 +22,7 @@ int dgrep_main(int argc, char *argv[]) {
   }
 
   size_t max_count = args.max_count_given ? args.max_count_arg : std::numeric_limits<size_t>::max();
+  size_t n_line = 0;
   std::string line, header;
   for(auto file : args.file_arg) {
     try {
@@ -31,21 +32,30 @@ int dgrep_main(int argc, char *argv[]) {
       int           c;
       // Display unchanged up to first header
       for(c = is.peek(); c != '>' && c != EOF; c = is.peek()) {
+        ++n_line;
         std::getline(is, line);
+        if(args.line_number_flag)
+          std::cout << n_line << ':';
         std::cout << line << '\n';
       }
 
       while(c != EOF && max_count > 0) {
+        const size_t header_n_line = ++n_line;
         std::getline(is, header);
         bool header_printed = false;
 
         for(c = is.peek(); c != '>' && c != EOF; c = is.peek()) {
+          ++n_line;
           std::getline(is, line);
           if(regex_search(line.cbegin(), line.cend(), regexp) ^ args.invert_match_flag) {
             if(!header_printed) {
+              if(args.line_number_flag)
+                std::cout << header_n_line << ':';
               std::cout << header << '\n';
               header_printed = true;
             }
+            if(args.line_number_flag)
+              std::cout << n_line << ':';
             std::cout << line << '\n';
             if(--max_count == 0) break;
           }
--- tests/test_dgrep.sh
+++ tests/test_dgrep.sh
@@ -4,3 +4,11 @@
 
 RES=$(ufasta dgrep 'CacGACgcggatGaGTcGcagAgccG.*GcGatcGcCtCacAGCAaGTtC' test1.fasta)
 EXPECT_EQ ">read6" $(echo "$RES" | head -n 1) "Got read6"
+
+RES=$(ufasta dgrep 'acgt' test1.fasta | grep -v '^>')
+GR=$(grep 'acgt' test1.fasta)
+EXPECT_EQ "$GR" "$RES" "Identical to grep"
+
+RES=$(ufasta dgrep -n 'act' test1.fasta | grep -v '>')
+GR=$(grep -n 'act' test1.fasta)
+EXPECT_EQ "$GR" "$RES" "Identical to grep, line numbers"
