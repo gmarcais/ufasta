8b89539a53a77d38b19c80e78412afe2094c3be4
Guillaume Marcais <gmarcais@cs.cmu.edu>
2022-09-06 14:49:57 -0400

    Better split testing
    * Test split on random sequences, with large number of output files
      (close to `ulimit -n`).

--- tests/test_split.sh
+++ tests/test_split.sh
@@ -1,5 +1,21 @@
 
-trap "rm -f split1 split2" EXIT
-ufasta split -i $TEST1 >(cat > split1) >(cat > split2)
+n=$(($(ulimit -n) - 20))
+n=$((n > 2000 ? 2000 : n))
+
+for i in $(seq 1 $n); do
+    oarray+=("splita$i")
+done
+
+split_clean() {
+    rm -f split1 split2 splita0 "${oarray[@]}"
+}
+trap split_clean EXIT
+
+set -x
+
+ufasta split -i "$TEST1" split1 split2
 diff <(cat split1 split2 | ufasta sort) <(ufasta sort $TEST1)
 EXPECT_SUCCESS "Split join"
+
+random_sequence | ufasta head -n $((n * 10)) | tee splita0 | ufasta split "${oarray[@]}"
+EXPECT_EQ "$(cat "${oarray[@]}"  | ufasta sort | md5sum)" "$(ufasta sort splita0 | md5sum)" "Split $n"
